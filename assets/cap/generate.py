cac820c153c672068f83ddff72962b88fa7b9012
Michael Whittaker <mjw297@cornell.edu>
2014-08-17 08:28:08 +0000

    Finished CAP theorem blog post.

--- assets/cap/generate.py
+++ assets/cap/generate.py
@@ -17,10 +17,36 @@ def main():
     
     draw("$v_0$", "$v_0$", "", "write $v_1$ $\\nwarrow$", "", True)
     draw("$v_1$", "$v_0$", "", "", "", True)
-    draw("$v_0$", "$v_0$", "", "done $\searrow$", "", True)
+    draw("$v_1$", "$v_0$", "", "done $\searrow$", "", True)
 
-    draw("$v_0$", "$v_0$", "", "", "", True)
-    draw("$v_0$", "$v_0$", "", "", "", True)
+    draw("$v_0$", "$v_0$", "", "read $\\nwarrow$", "", True)
+    draw("$v_0$", "$v_0$", "", "$v_0 \searrow$", "", True)
+    
+    draw("$v_0$", "$v_0$", "", "write $v_1$ $\\nwarrow$", "", True)
+    draw("$v_1$", "$v_0$", "", "", "", True)
+    draw("$v_1$", "$v_0$", "", "done $\\searrow$", "", True)
+    draw("$v_1$", "$v_0$", "", "", "$\\nearrow read$", True)
+    draw("$v_1$", "$v_0$", "", "", "$\\swarrow v_0$", True)
+    
+    draw("$v_0$", "$v_0$", "", "write $v_1$ $\\nwarrow$", "", True)
+    draw("$v_1$", "$v_0$", "", "", "", True)
+    draw("$v_1$", "$v_0$", "$v_1 \\rightarrow$", "", "", True)
+    draw("$v_1$", "$v_1$", "", "", "", True)
+    draw("$v_1$", "$v_1$", "$\\leftarrow$ done", "", "", True)
+    draw("$v_1$", "$v_1$", "", "done $\\searrow$", "", True)
+    draw("$v_1$", "$v_1$", "", "", "$\\nearrow read$", True)
+    draw("$v_1$", "$v_1$", "", "", "$\\swarrow v_1$", True)
+    
+    draw("$v_0$", "$v_0$", "", "", "", False)
+
+    draw("$v_0$", "$v_0$", "", "", "", False)
+    
+    draw("$v_0$", "$v_0$", "", "write $v_1$ $\\nwarrow$", "", False)
+    draw("$v_1$", "$v_0$", "", "", "", False)
+    draw("$v_1$", "$v_0$", "", "done $\searrow$", "", False)
+    
+    draw("$v_1$", "$v_0$", "", "", "$\\nearrow read$", False)
+    draw("$v_1$", "$v_0$", "", "", "$\\swarrow v_0$", False)
 
 if __name__ == "__main__":
     main()
