ffa319d8b5adafbccbe1b27df900748c52290f39
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-01-13 11:51:05 -0800

    Enabled unit testing again.

--- blog/visualizing_distributed_systems_consistency/ts/main.ts
+++ blog/visualizing_distributed_systems_consistency/ts/main.ts
@@ -1,7 +1,13 @@
+import "./assert_test";
+import "./pervasive_test";
+import "./queue_test";
+import "./range_test";
+import "./register_test";
+import "./schedule_test";
 import * as unittest from "./unittest";
 
 function main(): void {
   unittest.run_all();
 }
 
-window.onload = main;
+main();
